0fc7e75193d6d608ba35414a8babe115e253f996
krzysztofwieczorek4056 <krzysztofwieczorek4056@gmail.com>
2022-06-23 14:12:49 +0200

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double temperatureReading;
+        double result;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Temperature");
+        temperatureReading = input.nextDouble();
+
+        result =(temperatureReading * 9/5) + 32;
+        System.out.println("Temperature in Fahr: " + result + " F");
+
+    }
+}
