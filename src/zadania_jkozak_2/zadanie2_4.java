93580c7830260224dea6aeb9f2b2442b86f834e8
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-06-25 19:43:19 +0200

    add zadanie2_4, add zadanie2_5 incomplete for save

--- src/zadania_jkozak_2/zadanie2_4.java
+++ src/zadania_jkozak_2/zadanie2_4.java
@@ -1,9 +1,28 @@
 package zadania_jkozak_2;
 
+import java.util.Scanner;
+
 public class zadanie2_4 {
+
     public static void main(String[] args) {
         System.out.println("zadanie jvkozak 2.4");
+        int suma = 0;
+        boolean zeroFound = true;
+        while (zeroFound) {
 
+            Scanner scanner = new Scanner(System.in);
+            int lidzby = scanner.nextInt();
+
+            suma= suma+lidzby;
+
+            if (lidzby == 0) {
+
+
+                zeroFound = false;
+                System.out.println(suma);
+            }
+
+        }
     }
 }
 
---
+++ src/zadania_jkozak_2/zadanie2_5.java
@@ -0,0 +1,29 @@
+package zadania_jkozak_2;
+
+import java.util.Scanner;
+
+public class zadanie2_5 {
+    public static void main(String[] args) {
+        System.out.println("zadanie jvkozak zadanie2.5");
+        boolean numberZero = true;
+        int liczbaMax = 0;
+        int lidzbaMin = 0;
+        while (numberZero) {
+            Scanner scanner = new Scanner(System.in);
+            int liczby = scanner.nextInt();
+
+            if (liczby > lidzbaMin) {
+                lidzbaMin = liczby;
+            }
+            if (liczby < liczbaMax) {
+                liczbaMax = liczby;
+            }
+
+            if (liczby == 0) {
+                numberZero = false;
+                System.out.println(lidzbaMin + " " + liczbaMax);
+            }
+        }
+    }
+}
+
