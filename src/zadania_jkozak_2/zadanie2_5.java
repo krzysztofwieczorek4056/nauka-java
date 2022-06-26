c3f3158cb31c0f238379a2404fb6059bd72734c8
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-06-26 17:27:49 +0200

    update zadanie2_5

--- src/zadania_jkozak_2/zadanie2_5.java
+++ src/zadania_jkozak_2/zadanie2_5.java
@@ -1,29 +1,37 @@
 package zadania_jkozak_2;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 
 public class zadanie2_5 {
-    public static void main(String[] args) {
+    public static void main(String @NotNull [] args) {
         System.out.println("zadanie jvkozak zadanie2.5");
         boolean numberZero = true;
         int liczbaMax = 0;
-        int lidzbaMin = 0;
+        int liczbaMin = 0;
+        double srednia = 0;
+        int zeroEnd = Integer.parseInt(String.valueOf(0));
         while (numberZero) {
             Scanner scanner = new Scanner(System.in);
             int liczby = scanner.nextInt();
+            liczby= liczby-zeroEnd;
+            double suma = liczbaMin + liczbaMax;
+            srednia = suma/2;
 
-            if (liczby > lidzbaMin) {
-                lidzbaMin = liczby;
+            if (liczby < liczbaMin) {
+                liczbaMin = liczby;
             }
-            if (liczby < liczbaMax) {
+            if (liczby > liczbaMax) {
                 liczbaMax = liczby;
             }
 
-            if (liczby == 0) {
+            if (liczby == zeroEnd) {
                 numberZero = false;
-                System.out.println(lidzbaMin + " " + liczbaMax);
+                System.out.println("suma lidzby max i min : ");
+                System.out.println(suma);
+                System.out.println("srednia arytmetyczna sumy min max : ");
+                System.out.println(srednia);
             }
         }
-    }
-}
-
+    }}
