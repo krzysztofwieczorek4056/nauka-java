32aaf2a8a05723d01ffc73d4dbfc9511bff329f3
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-07-08 20:53:41 +0200

    add zadanie 3_5.

--- src/zadania_jkozak_3/zadanie3_5.java
+++ src/zadania_jkozak_3/zadanie3_5.java
@@ -4,67 +4,73 @@ import java.util.Scanner;
 
 public class zadanie3_5 {
     public static void main(String[] args) {
-        int liczba;
+        int liczbaZU1;
+        int liczbaZU2;
         int reszta;
         int[] tablica = new int[32];
+        int[] tablica2 = new int[32];
         int i = 0;
+        int j = 0;
         boolean pierwszy = true;
-        boolean znak = true;
+        boolean drugi = true;
         int licznikPetli = 0;
+        int licznikPetli2 = 0;
         int bitZnaku;
         System.out.println("zadanie jkozak 3.5");
         System.out.println("kropka po bicie znaku jest umowna dla lepszego odczytu");
         Scanner scanner = new Scanner(System.in);
-        liczba = scanner.nextInt();
-        if (liczba < 0) {
+        liczbaZU1 = scanner.nextInt();
+        liczbaZU2 = liczbaZU1 + 1;
+        if (liczbaZU1 < 0) {
             bitZnaku = 1;
         } else {
             bitZnaku = 0;
         }
-        while (liczba >= 1) {
+        System.out.println("to sa dzielniki dla sprawdzenia");
+        while (liczbaZU1 >= 1) {
 
             if (pierwszy) {
-                reszta = liczba % 2;
+                reszta = liczbaZU1 % 2;
                 tablica[i] = reszta;
-                System.out.println(liczba + " " + reszta);
+                System.out.println(liczbaZU1 + " " + reszta);
                 pierwszy = false;
                 i++;
                 licznikPetli++;
             }
-            if (liczba > 1) {
-                liczba = liczba / 2;
-                reszta = liczba % 2;
+            if (liczbaZU1 > 1) {
+                liczbaZU1 = liczbaZU1 / 2;
+                reszta = liczbaZU1 % 2;
                 tablica[i] = reszta;
                 i++;
                 licznikPetli++;
-                System.out.println(liczba + " " + reszta);
+                System.out.println(liczbaZU1 + " " + reszta);
             }
-            if (liczba == 1) {
+            if (liczbaZU1 == 1) {
                 break;
             }
         }
-        if (liczba > 0) {
+        if (liczbaZU1 > 0) {
             tablica[i] = bitZnaku;
             i++;
         }
-        while (liczba < 0) {
+        while (liczbaZU1 < 0) {
 
             if (pierwszy) {
-                reszta = -liczba % 2;
+                reszta = -liczbaZU1 % 2;
                 if (reszta == 0) {
                     reszta = reszta + 1;
                 } else {
                     reszta = reszta - 1;
                 }
                 tablica[i] = reszta;
-                System.out.println(liczba + " " + reszta);
+                System.out.println(liczbaZU1 + " " + reszta);
                 pierwszy = false;
                 i++;
                 licznikPetli++;
             }
-            if (liczba < -1) {
-                liczba = liczba / 2;
-                reszta = -liczba % 2;
+            if (liczbaZU1 < -1) {
+                liczbaZU1 = liczbaZU1 / 2;
+                reszta = -liczbaZU1 % 2;
                 if (reszta == 0) {
                     reszta = reszta + 1;
                 } else {
@@ -73,26 +79,66 @@ public class zadanie3_5 {
                 tablica[i] = reszta;
                 i++;
                 licznikPetli++;
-                System.out.println(liczba + " " + reszta);
+                System.out.println(liczbaZU1 + " " + reszta);
             }
 
-            if (liczba == -1) {
+            if (liczbaZU1 == -1) {
                 break;
             }
-
         }
-        if (liczba < 0) {
+        // ----------------------------------------------------------ZAPIS ZU2--------------------------------------
+        while (liczbaZU2 < 0) {
+
+            if (drugi) {
+                reszta = -liczbaZU2 % 2;
+                if (reszta == 0) {
+                    reszta = reszta + 1;
+                } else {
+                    reszta = reszta - 1;
+                }
+                tablica2[j] = reszta;
+                System.out.println(liczbaZU2 + " " + reszta);
+                drugi = false;
+                j++;
+                licznikPetli2++;
+            }
+            if (liczbaZU2 < -1) {
+                liczbaZU2 = liczbaZU2 / 2;
+                reszta = -liczbaZU2 % 2;
+                if (reszta == 0) {
+                    reszta = reszta + 1;
+                } else {
+                    reszta = reszta - 1;
+                }
+                tablica2[j] = reszta;
+                j++;
+                licznikPetli2++;
+                System.out.println(liczbaZU2 + " " + reszta);
+            }
+
+            if (liczbaZU2 == -1) {
+                break;
+            }
+        }
+//// ----------------------------------------------------------------------------------------ZAPIS ZU2--------------
+
+
+        if (liczbaZU1 < 0 || liczbaZU2 <0) {
             tablica[i] = bitZnaku;
+            tablica2[j] = bitZnaku;
+            j++;
             i++;
 
         }
-        if (liczba == 0) {
-            tablica[i] = liczba;
+        if (liczbaZU1 == 0 || liczbaZU2 ==0) {
+            tablica[i] = liczbaZU1;
+            tablica2[j] = liczbaZU1;
+            licznikPetli2++;
             licznikPetli++;
         }
 
-        System.out.println("gotowe rozwiazanie z wykorzystaniem tablicy ");
-        if (liczba > 0) {
+        System.out.println("gotowe rozwiazanie z wykorzystaniem tablicy  zapis ZU1");
+        if (liczbaZU1 > 0) {
             for (i = licznikPetli; i >= 0; i--) {
                 if (i == licznikPetli) {
                     System.out.print(tablica[i] + ".");
@@ -101,15 +147,27 @@ public class zadanie3_5 {
                 }
             }
         }
-        if (liczba < 0) {
+        if (liczbaZU1 < 0) {
             for (i = licznikPetli; i >= 0; i--) {
-                if (i == licznikPetli ) {
+                if (i == licznikPetli) {
                     System.out.print(tablica[i] + ".");
                 } else {
                     System.out.print(tablica[i]);
                 }
             }
         }
+        System.out.println();
+        System.out.println("zapis ZU2");
+        if (liczbaZU2 < 0) {
+            for (j = licznikPetli2; j >= 0; j--) {
+                if (j == licznikPetli2) {
+                    System.out.print(tablica2[j] + ".");
+                } else {
+                    System.out.print(tablica2[j]);
+                }
+            }
+        }
+
     }
 }
 //zapis uzupe³nienie do jednego to
@@ -121,3 +179,14 @@ public class zadanie3_5 {
 // -5 = 1.010
 //-99 = 0011100
 //99 = 1100011
+//-6 = 1.001
+// zapis zu2
+//-5 = 1.010 + 1
+//-5 = 1.011
+// -6 = 1.010
+// -7 = 1.001
+// dodawanie zu2
+//1+1 = 0 z przeniesieniem na nastêpny bit
+// 1+0 = 1
+// 0+1 = 1
+// do ostatniego i w petli dodaje 1
