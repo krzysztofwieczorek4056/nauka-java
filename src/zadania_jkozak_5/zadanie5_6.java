3a0210da8759f26f78f1d8f71fd83886b8637d36
Krzysztof <krzysztofwieczorek40564056@gmail.com>
2022-08-22 22:57:45 +0200

    Poprawka XD

--- src/zadania_jkozak_5/zadanie5_6.java
+++ src/zadania_jkozak_5/zadanie5_6.java
@@ -14,21 +14,26 @@ public class zadanie5_6 {
         StringBuilder bolid = new StringBuilder();
         int pusty;
         pusty = 0;
+        boolean isNegative = false;
         int pusty1;
         char[] tab = text.toCharArray();
         for (char s : tab) {
             if (s == 43 || s == 45) {
-
+                isNegative = true;
             } else if (s > 47 && s < 58) {
                 bolid.append(s);
             } else {
                 break;
             }
-            pusty1 = Integer.parseInt(bolid.toString());
-            pusty = pusty1;
-            if (tab[0] ==43){
-                pusty = -pusty;
-            }
+
+        }
+        if(isNegative){
+            bolid.insert(0,"-");
+        }
+        pusty1 = Integer.parseInt(bolid.toString());
+        pusty = pusty1;
+        if (tab[0] ==43){
+            pusty = -pusty;
         }
         return pusty;
     }
