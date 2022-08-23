f1ea9062226cba73509c1b1ffa3de9937127fbb3
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-08-23 20:51:34 +0200

    add end zadanie 5.7

--- src/zadania_jkozak_5/zadanie5_7.java
+++ src/zadania_jkozak_5/zadanie5_7.java
@@ -9,33 +9,31 @@ public class zadanie5_7 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("podaj text");
         String what = scanner.nextLine();
-        System.out.println("podaj co chcesz znaleŸæ w tekscie");
+        System.out.println("podaj co chcesz znalezc w tekscie");
         String where = scanner.nextLine();
         System.out.println(starFind(what,where));
     }
     public static int starFind(String what, String where) {
         int positionIndex = 0;
-        boolean none = where.isBlank();
         boolean cont = what.contains(where);
-if (cont){
-    String first = String.valueOf(where.charAt(0));
-    String[] tab = what.split("");
-    for (int i =0;i<what.length();i++){
-        if (Objects.equals(tab[i], first)){
-            positionIndex = i;
+        if (where.equals("")){
+            return positionIndex;
         }
-    }
-    
-}
- else if (none) {
+        if (cont){
+            String first = String.valueOf(where.charAt(0));
+            String[] tab = what.split("");
+            for (int i =0;i<what.length();i++){
+                if (Objects.equals(tab[i], first)){
+                    positionIndex = i;
+                }
+            }
 
-     positionIndex = 0;
-}
+        }
 
-else {
-    positionIndex =-1;
-}
-return positionIndex;
+        else {
+            positionIndex =-1;
+        }
+        return positionIndex;
 
     }
 }
