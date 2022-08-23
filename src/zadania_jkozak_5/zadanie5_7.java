4899c5332feb5e1ebb52abdcb3d86ac206e22bb2
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-08-23 20:18:46 +0200

    add zadanie 5.7

---
+++ src/zadania_jkozak_5/zadanie5_7.java
@@ -0,0 +1,40 @@
+package zadania_jkozak_5;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class zadanie5_7 {
+    public static void main(String[] args) {
+        System.out.println("zadanie 5.7 jkozak");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj text");
+        String what = scanner.nextLine();
+        System.out.println("podaj co chcesz znaleŸæ w tekscie");
+        String where = scanner.nextLine();
+        System.out.println(starFind(what,where));
+    }
+    public static int starFind(String what, String where) {
+        int positionIndex = 0;
+        boolean none = where.isBlank();
+        boolean cont = what.contains(where);
+if (cont){
+    String first = String.valueOf(where.charAt(0));
+    String[] tab = what.split("");
+    for (int i =0;i<what.length();i++){
+        if (Objects.equals(tab[i], first)){
+            positionIndex = i;
+        }
+    }
+    
+}
+ else if (none) {
+     positionIndex = 0;
+}
+
+else {
+    positionIndex =-1;
+}
+return positionIndex;
+
+    }
+}
