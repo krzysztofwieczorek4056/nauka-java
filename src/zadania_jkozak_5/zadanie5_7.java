eb4361649424817f5e29fbbea116895c1f3ccc52
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-08-23 20:51:53 +0200

    add end zadanie 5.7

--- src/zadania_jkozak_5/zadanie5_7.java
+++ src/zadania_jkozak_5/zadanie5_7.java
@@ -11,27 +11,27 @@ public class zadanie5_7 {
         String what = scanner.nextLine();
         System.out.println("podaj co chcesz znalezc w tekscie");
         String where = scanner.nextLine();
-        System.out.println(starFind(what,where));
+        System.out.println(starFind(what, where));
     }
+
     public static int starFind(String what, String where) {
         int positionIndex = 0;
         boolean cont = what.contains(where);
-        if (where.equals("")){
+        if (where.equals("")) {
             return positionIndex;
         }
-        if (cont){
+        if (cont) {
             String first = String.valueOf(where.charAt(0));
             String[] tab = what.split("");
-            for (int i =0;i<what.length();i++){
-                if (Objects.equals(tab[i], first)){
+            for (int i = 0; i < what.length(); i++) {
+                if (Objects.equals(tab[i], first)) {
                     positionIndex = i;
+
                 }
             }
 
-        }
-
-        else {
-            positionIndex =-1;
+        } else {
+            positionIndex = -1;
         }
         return positionIndex;
 
