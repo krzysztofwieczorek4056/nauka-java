ffdbab2a47cc23656855a2a0eb10344857239a3d
krzysztof.wieczorek4056 <krzysztof.wieczorek4056@gmail.com>
2022-10-14 19:19:53 +0200

    Poprawka do 6.1

--- src/zadania_jkozak_6/zadanie6_1.java
+++ src/zadania_jkozak_6/zadanie6_1.java
@@ -2,29 +2,32 @@ package zadania_jkozak_6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class zadanie6_1 {
 
-    static String text;
-    static int[] tabInt;
+
 
     public static void main(String[] args) throws FileNotFoundException {
+
+         String text = "";
+         int[] tabInt = new int[3];
+
         System.out.println("zadanie j.kozak 6.1");
-        File plik = new File("C:\\Users\\Cezary\\Desktop\\plik.TXT");
+        File plik = new File("C:\\Users\\krzys\\Desktop\\test.txt");
         Scanner scanner = new Scanner(plik);
         while (scanner.hasNext()) {
-            text = scanner.nextLine();
-
+            text += scanner.nextLine();
         }
-        System.out.println((Arrays.toString(liczZnakiSlowa(Arrays.toString(tabInt)))));
 
+        int[] result = liczZnakiSlowa(text, tabInt);
+        for(int a : result){
+            System.out.println(a);
+        }
     }
 
-    public static int[] liczZnakiSlowa(String a) {
-        int[] tabInt = new int[3];
-        System.out.println(text);
+    public static int[] liczZnakiSlowa(String text, int[] tabInt) {
+        //System.out.println(text);
         String text1 = text.trim();
         String[] tab = text1.split(" ");
         int dlugoscZnakow = text1.length() - tab.length + 1;
@@ -48,9 +51,10 @@ public class zadanie6_1 {
         int wordy = 0;
         String text3 = text.trim();
         String[] words = text3.split("\\W");
-        for (String w : words) {
-            wordy++;
-        }
+        wordy += words.length;
+//        for (String w : words) {
+//            wordy++;
+//        }
         tabInt[2] = wordy;
         return tabInt;
     }
