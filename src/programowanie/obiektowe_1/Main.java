f93a9ad67287bc7ea99d51e5c7b752dac271c78a
CezaryProgramista <cezary.konowrocki@wp.pl>
2022-11-14 22:23:49 +0100

    start zadanie obiektowe

---
+++ src/programowanie/obiektowe_1/Main.java
@@ -0,0 +1,9 @@
+package programowanie.obiektowe_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Stuff stuff = new Stuff();
+        stuff.toString();
+
+    }
+}
---
+++ src/programowanie/obiektowe_1/Stuff.java
@@ -0,0 +1,19 @@
+package programowanie.obiektowe_1;
+
+public class Stuff {
+    private String name;
+    private int weight;
+
+    public Stuff() {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        System.out.println(name +" "+ weight);
+        return null;
+    }
+
+
+}
